756ae94852a91c629d7cdcccc55620914b5a133a
vincent861223 <B05505019@ntu.edu.tw>
2017-03-28 01:15:03 +0800

    Initial Commit

---
+++ Insertion Sort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Insertion Sort
+//
+//  Created by 晉辰 林 on 28/03/2017.
+//  Copyright © 2017 晉辰 林. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
